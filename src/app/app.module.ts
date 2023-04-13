29d2e162339f5d8522aea1da4c3255b65eed6ffe
DESKTOP-DTFO15Q\Akiba <kredynets.a@gmail.com>
2023-04-13 09:22:15 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {DxBulletModule, DxDataGridModule, DxTemplateModule} from "devextreme-angular";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DxDataGridModule,
+    DxTemplateModule,
+    DxBulletModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
